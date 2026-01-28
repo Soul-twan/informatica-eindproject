aed81a20cfdec5df9032898112bbca0b7ff94faf
Edd1h <hidde.groenevelt@gmail.com>
2026-01-28 08:55:45 +0100

    arrow keys added

--- player.gd
+++ player.gd
@@ -8,11 +8,11 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_key_pressed(KEY_A):
+	if Input.is_key_pressed(KEY_A) or Input.is_action_pressed("ui_left"):
 		position += Vector2(-400,0) * delta
-	if Input.is_key_pressed(KEY_D):
+	if Input.is_key_pressed(KEY_D) or Input.is_action_pressed("ui_right"):
 		position += Vector2(400,0) * delta
-	if Input.is_key_pressed(KEY_S):
+	if Input.is_key_pressed(KEY_S) or Input.is_action_pressed("ui_down"):
 		position += Vector2(0, 400) * delta
-	if Input.is_key_pressed(KEY_W):
+	if Input.is_key_pressed(KEY_W) or Input.is_action_pressed("ui_up"):
 		position += Vector2(0, -400) * delta
