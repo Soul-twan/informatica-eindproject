84e115f96373dd15360c9ff395c2f608a4b68822
Edd1h <hidde.groenevelt@gmail.com>
2026-01-28 09:55:21 +0100

    Update player.gd

--- player.gd
+++ player.gd
@@ -1,18 +1,31 @@
 extends Node2D
 
+var speed = 200
+var acceleration  = 1.06
+var moving = false
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	position = Vector2(400,400)
+	
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_key_pressed(KEY_A) or Input.is_action_pressed("ui_left"):
-		position += Vector2(-400,0) * delta
-	if Input.is_key_pressed(KEY_D) or Input.is_action_pressed("ui_right"):
-		position += Vector2(400,0) * delta
-	if Input.is_key_pressed(KEY_S) or Input.is_action_pressed("ui_down"):
-		position += Vector2(0, 400) * delta
-	if Input.is_key_pressed(KEY_W) or Input.is_action_pressed("ui_up"):
-		position += Vector2(0, -400) * delta
+	if moving == true:
+		if speed <= 400:
+			speed = speed * acceleration
+		if speed >= 400:
+			speed = 400
+	if Input.is_key_pressed(KEY_A) or Input.is_action_pressed("ui_left"): # A or left arrow\
+		moving = true
+		position += Vector2(-speed,0) * delta
+	if Input.is_key_pressed(KEY_D) or Input.is_action_pressed("ui_right"): # D or right arrow
+		moving = true
+		position += Vector2(speed,0) * delta
+	if Input.is_key_pressed(KEY_S) or Input.is_action_pressed("ui_down"): # S or arrow down
+		moving = true
+		position += Vector2(0, speed) * delta
+	if Input.is_key_pressed(KEY_W) or Input.is_action_pressed("ui_up"): # W or arrow up
+		moving = true
+		position += Vector2(0, -speed) * delta
+		
+	if Input.is_action_just_pressed("ui_select"): # spacebar
+		position += Vector2(0,-100)
