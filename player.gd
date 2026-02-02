2e419a86d235d0fed28c82ca0c59e37a5e140ca6
Edd1h <hidde.groenevelt@gmail.com>
2026-02-02 21:23:00 +0100

    added actual acc and collision

--- player.gd
+++ player.gd
@@ -1,31 +1,25 @@
-extends Node2D
+extends CharacterBody2D
 
-var speed = 200
-var acceleration  = 1.06
-var moving = false
+const max_speed = 400.0
+const acc = 30
+const fric = 70
+const jump_power = -600.0
 
-func _ready() -> void:
-	position = Vector2(600,520)
-	
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * 2 * delta
 
-func _process(delta: float) -> void:
-	if moving == true:
-		if speed <= 400:
-			speed = speed * acceleration
-		if speed >= 400:
-			speed = 400
-	if Input.is_key_pressed(KEY_A) or Input.is_action_pressed("ui_left"): # A or left arrow\
-		moving = true
-		position += Vector2(-speed,0) * delta
-	if Input.is_key_pressed(KEY_D) or Input.is_action_pressed("ui_right"): # D or right arrow
-		moving = true
-		position += Vector2(speed,0) * delta
-	if Input.is_key_pressed(KEY_S) or Input.is_action_pressed("ui_down"): # S or arrow down
-		moving = true
-		position += Vector2(0, speed) * delta
-	if Input.is_key_pressed(KEY_W) or Input.is_action_pressed("ui_up"): # W or arrow up
-		moving = true
-		position += Vector2(0, -speed) * delta
-		
-	if Input.is_action_just_pressed("ui_select"): # spacebar
-		position += Vector2(0,-100)
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = jump_power
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	if input:
+		velocity.x = move_toward(velocity.x, input * max_speed, acc)
+	else:
+		velocity.x = move_toward(velocity.x, 0, fric)
+
+	move_and_slide()
