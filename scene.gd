7942e37d220c3e9651ccb32e8f3cb2442e06df3e
Soul_ <98612367+Soul-twan@users.noreply.github.com>
2026-03-04 12:30:46 +0100

    Tutorial fully implemented

--- scene.gd
+++ scene.gd
@@ -7,8 +7,25 @@ var Dash
 var WallJump
 var DoubleJump
 
+@onready var textBg = $Camera/Textbg
+@onready var tutorialBg = $Camera/TutorialBg
+@onready var tutorialHUDBottom = $Camera/TutorialHUDBottom
+@onready var tutorialHUDTop = $Camera/TutorialHUDTop
+
 const tutorialText = [
-	"Press \"e\"  to continue text boxes",
+	"Press \"e\"  to continue through text boxes",
+	"It seems I got myself stuck in a time loop.",
+	"I'll have to find time crystals to",
+	"recharge my time watch and get back", 
+	"home.",
+	"I can move around with the left and",
+	"right arrow keys and jump with \"c\".",
+	"I can look up and down with the up and",
+	"down arrow keys.",
+	"I could wall jump, double jump and",
+	"dash with \"x\" or \"v\" , but I think I need",
+	"to recharge my time watch for that too.",
+	""
 ]
 
 # Called when the node enters the scene tree for the first time.
@@ -20,11 +37,6 @@ func _ready() -> void:
 	healthHUD.visible = true
 	var HUDbg = get_node("Camera/HUDbg")
 	HUDbg.visible = true
-	
-	var textBg = $Camera/Textbg
-	var tutorialBg = $Camera/TutorialBg
-	var tutorialHUDBottom = $Camera/TutorialHUDBottom
-	var tutorialHUDTop = $Camera/TutorialHUDTop
 	
 	if Dash in player.inventory:
 		timer.wait_time = 30.0
@@ -52,7 +64,6 @@ func _process(_delta: float) -> void:
 		loopReset()
 		
 	tutorial()
-	
 
 func _on_timer_timeout() -> void:
 	loopReset()
@@ -73,8 +84,39 @@ func tutorial():
 		player.inventory["TutorialPart"] = 0
 	
 	if Input.is_action_just_pressed("ui_pickup"):
-		player.inventory["TutorialPart"] += 1
+		if player.inventory["TutorialPart"] < 7:
+			player.inventory["TutorialPart"] += 1
 		
-	match player.inventory["TutorialPart"]:
-		0:
-			pass
+	if !"TutorialCompletion" in player.inventory:
+		match player.inventory["TutorialPart"]:
+			0:
+				textBg.visible = true
+				tutorialHUDBottom.visible = true
+				tutorialHUDBottom.text = tutorialText[0]
+			1:
+				textBg.visible = false
+				tutorialBg.visible = true
+				tutorialHUDTop.visible = true
+				tutorialHUDTop.text = tutorialText[1]
+				tutorialHUDBottom.text = tutorialText[2]
+			2:
+				tutorialHUDTop.text = tutorialText[3]
+				tutorialHUDBottom.text = tutorialText[4]
+			3:
+				tutorialHUDTop.text = tutorialText[5]
+				tutorialHUDBottom.text = tutorialText[6]
+			4:
+				tutorialHUDTop.text = tutorialText[7]
+				tutorialHUDBottom.text = tutorialText[8]
+			5:
+				tutorialHUDTop.text = tutorialText[9]
+				tutorialHUDBottom.text = tutorialText[10]
+			6:
+				tutorialHUDTop.text = tutorialText[11]
+				tutorialHUDBottom.text = tutorialText[12]
+			7:
+				tutorialBg.visible = false
+				tutorialHUDTop.visible = false
+				tutorialHUDBottom.visible = false
+				player.inventory["TutorialCompletion"] = 1
+				loopReset()
